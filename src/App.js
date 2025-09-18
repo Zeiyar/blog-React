ef9926523a26814bfd04ceded1a9f246353b92da
Zeiyar <marieycorentin97@gmail.com>
2025-09-18 09:54:09 +0200

    last repair

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home"
-import Login from "./pages/login"
-import Register from "./pages/register"
-import DashBoard from "./pages/dashboard"
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import DashBoard from "./pages/Dashboard";
 
 function App() {
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/login" element={<Login />}/>
-      <Route path="/register" element={<Register />}/>
-      <Route path="/dashboard" element={<DashBoard />}/>
-    </Routes>
-  </Router>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<Register />}/>
+        <Route path="/dashboard" element={<DashBoard />}/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
